388c1f6fe353072dcaf9c2e8a7fc59cbebc196bd
porithe <malkowski.buisness@gmail.com>
2019-06-20 20:58:41 +0200

    random bitcoin price

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,19 @@
 const ctx = document.getElementById('myChart').getContext('2d');
-  
-let canvasArea = document.getElementById('myChart');
+const bitcoinTag = document.getElementById('btc-tag');
+
+
+let labelsUpper = 1;
+let bitcoinStartValue  = 7500;
+
+bitcoinTag.textContent = bitcoinStartValue;
+
 
 const myChart = new Chart(ctx, {
   type: 'line',
   data: {
-    labels: ['January', 'February', 'March', 'April', 'May'],
+    labels: [labelsUpper],
     datasets: [{
-      data: [10, 15, 29, 6, 18],
+      data: [bitcoinStartValue],
       backgroundColor: ['rgba(33,150,243 ,0.3)'],
       borderColor: ['rgba(33,150,243 ,1)'],
       borderWidth: 2,
@@ -29,13 +35,42 @@ const myChart = new Chart(ctx, {
   }
 });
 
+setInterval( () => {
+  labelsUpper++;
 
-const test = document.querySelector('.title');
+  let bitcoinTmpValue = bitcoinStartValue;
 
-test.addEventListener('click', function() {
-  myChart.data.datasets[0].data.push(50);
-  myChart.data.labels.push('June')
-  console.log('kappa');
-  console.log(myChart.data.datasets[0].data);
+
+  let max = 1.40;
+  let min = 0.70;
+
+  if (bitcoinStartValue > 25000) {
+    max = 1.25;
+    max = 0.55;
+  }
+  if (bitcoinStartValue < 1500) {
+    max = 1.6;
+    min = 0.80;
+  }
+
+  let randomNumber = Math.random() * (1.40 - 0.70) + 0.70;
+
+  bitcoinStartValue = Math.floor(bitcoinStartValue * randomNumber);
+  bitcoinTag.textContent = bitcoinStartValue;
+  if (bitcoinTmpValue < bitcoinStartValue) {
+    bitcoinTag.style.color = '#4CAF50';
+  }
+  else {
+    bitcoinTag.style.color = '#f44336';
+  }
+  myChart.data.labels.push(labelsUpper);
+  myChart.data.datasets[0].data.push(bitcoinStartValue);
+
+  if (myChart.data.labels.length >= 50) {
+    myChart.data.labels.shift();
+    myChart.data.datasets[0].data.shift();
+  }
+
   myChart.update();
-});
+  console.log('kappa');
+}, 2000);
